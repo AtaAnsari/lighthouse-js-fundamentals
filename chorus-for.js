1a6476a487a1a190c78566e18b3be48bb23d8230
AtaAnsari <ataulhaq.ansari@gmail.com>
2019-12-27 09:17:45 +0000

    updated with for loop excercise

---
+++ chorus-for.js
@@ -0,0 +1,8 @@
+const chorus = "Let's dance!"
+for (let repeat=0; repeat < 10; repeat++){
+  console.log(chorus);
+  if (repeat === 5) {
+  console.log("*change key*");
+  }
+}
+console.log("Until the sun comes up!");
