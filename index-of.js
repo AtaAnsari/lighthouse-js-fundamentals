5bb6bd5c45fc0866eb46260fb5f480d0e14721b9
AtaAnsari <ataulhaq.ansari@gmail.com>
2020-01-03 13:55:38 +0000

    index-of assigment, looping over arrays

---
+++ index-of.js
@@ -0,0 +1,14 @@
+const lastIndexOf = function (array, num) {
+  for (let i = array.length - 1; i >= 0; i--){
+    if(num === array[i]){
+      return i 
+    }
+  }
+  return -1
+}
+
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 1), "=?", 3);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 2), "=?", 4);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 3), "=?", -1);
+console.log(lastIndexOf([ 5, 5, 5 ], 5), "=?", 2);
+console.log(lastIndexOf([], 3), "=?", -1); 
