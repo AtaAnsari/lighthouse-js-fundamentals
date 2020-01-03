3d9eeb5ba2823e72ab836e73e693ab4f396f7145
AtaAnsari <ataulhaq.ansari@gmail.com>
2020-01-03 14:07:50 +0000

    concat excercise - arrays

---
+++ concat.js
@@ -0,0 +1,9 @@
+const concat = function(array1, array2){
+  let newarray = array1.concat(array2);
+  return newarray;
+}
+
+console.log(concat([ 1, 2, 3 ], [ 4, 5, 6 ]), "=?", [ 1, 2, 3, 4, 5, 6 ]);
+console.log(concat([ 0, 3, 1 ], [ 9, 7, 2 ]), "=?", [ 0, 3, 1, 9, 7, 2 ]);
+console.log(concat([], [ 9, 7, 2 ]), "=?", [ 9, 7, 2 ]);
+console.log(concat([ 5, 10 ], []), "=?", [ 5, 10 ]);
